08f68c1f16e0346265eadbf0077daaf2931089d8
Shaurya Bisht <87357655+ShauryaDusht@users.noreply.github.com>
2025-04-08 23:09:39 +0530

    updated static dir

--- collaborative_code_editor/settings.py
+++ collaborative_code_editor/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
